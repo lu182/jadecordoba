e197529b1b77ab92ccf74fa8fa0a1dd10285fbb9
Luciana <laf.0182@gmail.com>
2022-10-25 17:12:27 -0300

    Integrador JS: select + packs

--- src/js/cuadrosPacks-functions.js
+++ src/js/cuadrosPacks-functions.js
@@ -7,10 +7,10 @@ function replaceText2(pack){
 }
 
 function packOver(pack){
-    if(pack === pack1){
+    if(pack.id === 'pack1'){
         pack.classList.replace('bg-muted', 'bg-primary')
         replaceText1(pack)
-    }else if(pack === pack2){
+    }else if(pack.id === 'pack2'){
         pack.classList.replace('bg-muted', 'bg-success')
         replaceText1(pack)
     }else{
@@ -21,10 +21,10 @@ function packOver(pack){
 }
 
 function packLeave(pack){
-    if(pack === pack1){
+    if(pack.id === 'pack1'){
         pack.classList.replace('bg-primary', 'bg-muted')
         replaceText2(pack)
-    }else if(pack === pack2){
+    }else if(pack.id === 'pack2'){
         pack.classList.replace('bg-success', 'bg-muted')
         replaceText2(pack)
     }else{
@@ -35,10 +35,17 @@ function packLeave(pack){
 }
 
 function packClick(pack){
-    pack.ariaChecked = true;
-    pack.classList.replace('bg-primary', 'bg-danger')
-    pack.classList.replace('bg-success', 'bg-danger')
-    pack.classList.replace('bg-warning', 'bg-danger')
+    pack.ariaChecked = true;    
+    pack.classList.replace('bg-primary', 'bg-danger')    
+    pack.classList.replace('bg-success', 'bg-danger')    
+    pack.classList.replace('bg-warning', 'bg-danger')    
+
+    pack.classList.replace('bg-muted','bg-danger')
+    select.value =
+        pack.id === 'pack1' && '1' ||
+        pack.id === 'pack2' && '2' ||
+        pack.id === 'pack3' && '3'
+
 
     for(let item of packages){
         if(item.id !== pack.id){            
---
+++ src/js/selectionPack-functions.js
@@ -0,0 +1,8 @@
+function clickedSelection(){
+    select.value === '1' && packClick('pack1');
+    select.value === '2' && packClick('pack2');
+    select.value === '3' && packClick('pack3');
+}
+
+
+select.addEventListener('change', clickedSelection)
