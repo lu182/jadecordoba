87e7e91d9b8cefe1ac20a1654cf46fad46e07f97
Luciana <laf.0182@gmail.com>
2022-10-24 23:35:47 -0300

    Integrador JS: Registro y almaceno elementos del Dom

---
+++ src/js/dom.js
@@ -0,0 +1,21 @@
+// Cuadros Packs
+const pack1 = document.getElementById('pack1')
+const pack2 = document.getElementById('pack2')
+const pack3 = document.getElementById('pack3')
+const packages = document.getElementsByClassName('card')
+
+// Valor de Pack
+const packValueWDiscount = document.getElementById('valorPackSinDto')
+
+// Formulario
+const form = document.forms.formulario
+
+// Select
+const select = document.querySelector('#seleccionPack')
+
+// Total A Pagar
+const totalToPay = document.getElementById('totalAPagar')
+
+// Botones
+const deleteBtn = document.getElementById('btnBorrar')
+const summaryBtn = document.getElementById('btnResumen')
